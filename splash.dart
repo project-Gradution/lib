a0b5acbae52828ad5fd7aee43deffaafa2791f0d
AZOSDQ <112640785+AZOSDQ@users.noreply.github.com>
2022-09-08 21:12:42 +0300

    Add files via upload

---
+++ splash.dart
@@ -0,0 +1,59 @@
+import 'package:camelapp/Home.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(const Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const MyHomePage(
+                  title: '',
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Color.fromARGB(255, 229, 208, 174),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Image.asset(
+                  'assets/images/Logo.png',
+                  width: 330,
+                  height: 150,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "جملي",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
