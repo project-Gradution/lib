a88ecb66510c257811768d430e4fc1873e7b4ec9
Sulaiman-Alharbi <100804967+Sulaiman-Alharbi@users.noreply.github.com>
2022-09-15 19:56:12 +0300

    Update HelpCenter.dart

--- HelpCenter.dart
+++ HelpCenter.dart
@@ -3,8 +3,8 @@ import 'package:demo1/Widgets/CustomEndDrawer.dart';
 import 'package:flutter/material.dart';
 import 'BottomNavigator.dart';
 
-const Mainbrown = Color.fromRGBO(137, 115, 88, 1);
-const Mainbeige = Color.fromRGBO(230, 203, 160, 1);
+const Mainbrown = const Color.fromRGBO(137, 115, 88, 1);
+const Mainbeige = const Color.fromRGBO(255, 240, 199, 1);
 final scaffoldKey = GlobalKey<ScaffoldState>();
 
 class HelpCenter extends StatefulWidget {
